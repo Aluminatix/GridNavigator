655d35c9a2b9c1e7107d99f3700b897b23e1df44
Aluminatix <p4as3nj85@gmail.com>
2024-10-18 00:02:43 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/js/dist/dropdown";
+import PathfindingVisualizer from "./pathfindingVisualizer/pathfindingVisualizer";
 
 
 function App() {
   
   return (
     <>
-      
+      <PathfindingVisualizer />
     </>
   )
 }
